bebe3a6b99a199fab7f2eae594dcebb1c0717e6f
alicelesowski <35936264+alicelesowski@users.noreply.github.com>
2018-09-17 17:41:04 -0600

    Update Mathematica.java

--- Mathematica.java
+++ Mathematica.java
@@ -15,15 +15,16 @@ public class Mathematica implements IMathematics {
     /**
      * 
      */
-    public void mathSort() {
-        // TODO implement here
+    public void mathSort(int[] array) {	//takes array into method
+		for(int i=1; i<array.length; i++){
+			while(i>0 && array[i-1] > array[i]){	//checks if previous element is greater than the original element being checked
+				int temp=array[i-1];
+				array[i-1]=array[i];
+				array[i]=temp;
+				i--;
+			}
+				
+		}
+	}
+    
     }
-
-    /**
-     * 
-     */
-    public void mathSort() {
-        // TODO implement here
-    }
-
-}
